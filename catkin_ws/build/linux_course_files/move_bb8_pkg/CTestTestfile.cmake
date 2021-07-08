a6a7c9aa58d70f6ac14e08a2855dc39eaac6d503
Tony Wang <anthonyswang@gmail.com>
2021-07-07 21:49:10 -0700

    with catkin: commit 2

---
+++ catkin_ws/build/linux_course_files/move_bb8_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/catkin_ws/src/linux_course_files/move_bb8_pkg
+# Build directory: /home/user/catkin_ws/build/linux_course_files/move_bb8_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
